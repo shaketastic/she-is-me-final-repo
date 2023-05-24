0a5ceeb20e3ee1c01c6a8ce68fd5113d337c8c38
shareekaepps <shareekaepps@icloud.com>
2023-05-24 17:06:25 -0400

    fixed index page

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,19 +1,32 @@
+// import Blogs from "../components/Blogs";
+
+// export default function Index() {
+//     return (
+//       <div className="single">
+//         <div className="content">
+//         <img src="https://www.schoolstickers.com/media/catalog/category/BANNER-anti-bullying.png" alt="banner"/>
+//         <div className="info">
+//         <span>Sherry</span>
+//         <p>Posted 2 days ago</p>
+//         </div>
+//         Index page
+//         {/* <Blogs /> */}
+      
+//       <div className="menu">m</div>
+//     </div></div>
+//     );
+//   }
+
 import Blogs from "../components/Blogs";
 
 export default function Index() {
-    return (
-      <div className="single">
-        <div className="content">
-        <img src="https://www.schoolstickers.com/media/catalog/category/BANNER-anti-bullying.png" alt="banner"/>
-        <div className="info">
-        <span>Sherry</span>
-        <p>Posted 2 days ago</p>
-        </div>
-        Index page
-        {/* <Blogs /> */}
-      </div>
-      <div className="menu">m</div>
+  return (
+    <div className="single">
+      {/* <h2>Index</h2> */}
+      <Blogs />
     </div>
-    );
-  }
+  );
+}
+
+
   

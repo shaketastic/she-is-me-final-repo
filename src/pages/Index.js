04fca698f696dde3128f77478f1eea1eb6ef0ada
shareekaepps <shareekaepps@icloud.com>
2023-05-24 16:34:30 -0400

    css fix, margin, oadding, flexbox

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -2,10 +2,18 @@ import Blogs from "../components/Blogs";
 
 export default function Index() {
     return (
-      <div>
+      <div className="single">
+        <div className="content">
+        <img src="https://www.schoolstickers.com/media/catalog/category/BANNER-anti-bullying.png" alt="banner"/>
+        <div className="info">
+        <span>Sherry</span>
+        <p>Posted 2 days ago</p>
+        </div>
         Index page
-        <Blogs />
+        {/* <Blogs /> */}
       </div>
-    )
+      <div className="menu">m</div>
+    </div>
+    );
   }
   

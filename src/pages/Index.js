8a07f467797a91ca240c40bf9463f9fafe6e2399
shareekaepps <shareekaepps@icloud.com>
2023-05-24 17:29:44 -0400

    fixed login/register page links

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -7,7 +7,7 @@
 //         <img src="https://www.schoolstickers.com/media/catalog/category/BANNER-anti-bullying.png" alt="banner"/>
 //         <div className="info">
 //         <span>Sherry</span>
-//         <p>Posted 2 days ago</p>
+//         <p>Posted 2 days ago {blogs.created_at}</p>
 //         </div>
 //         Index page
 //         {/* <Blogs /> */}
@@ -21,8 +21,8 @@ import Blogs from "../components/Blogs";
 
 export default function Index() {
   return (
-    <div className="single">
-      {/* <h2>Index</h2> */}
+    <div className="blogs">
+      <h2>Index</h2>
       <Blogs />
     </div>
   );
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function Register() {
     return (
-      // <div className="container"> 
+      <div className="container"> 
             <div className="auth">
             <h1>Register</h1>
   
@@ -13,10 +13,10 @@ export default function Register() {
               <button>Register</button>
 
               <p>Sorry there is an error!</p>
-              <span>Do you have an account? <Link to="/login">Login</Link>
+              <span>Do you have an account? <Link to="/blogs/login">Login</Link>
               </span>
           </form>
       </div>
-    // </div>
+    </div>
     )
   }
